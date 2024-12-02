208e2ddc88874e2c609237101fb1a23cbb6e601f
Jessie Zhang <55704245+jessieziyun@users.noreply.github.com>
2024-12-02 09:49:04 +0800

    remove website info

--- components/page-footer.vue
+++ components/page-footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid grid-cols-4 gap-4 mt-20 items-end">
-    <div class="col-span-2">
+    <div class="col-span-3">
       <p>
         <a href="https://www.are.na/jessie-zhang" target="_blank" class="link"
           >Are.na</a
@@ -18,15 +18,12 @@
         >
       </p>
     </div>
-    <a
-      href="mailto:jessie.a.zhang@gmail.com"
-      target="_blank"
-      class="hidden sm:block link"
-      >Email</a
-    >
     <div class="text-right col-span-2 sm:col-span-1">
-      <nuxt-link to="/website" class="link text-right"
-        >Website <br class="sm:hidden"/>Information</nuxt-link
+      <a
+        href="mailto:jessie.a.zhang@gmail.com"
+        target="_blank"
+        class="link"
+        >Email</a
       >
     </div>
   </div>
